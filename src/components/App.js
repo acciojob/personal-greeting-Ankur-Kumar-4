0d3fba3d66428967be1adfdcff0710342c3beba0
Ankurdyno <98178905+Ankurdyno@users.noreply.github.com>
2023-12-11 20:12:31 +0530

    v-4

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,16 @@
-
-import React,{useState} from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-
-  const [greet, setGreet] = useState("")
+  const [greet, setGreet] = useState("");
 
   return (
     <div>
-        <p>Enter your name:</p>
-        <input type="text" onChange={(e)=>setGreet(e.target.value)}/>
-        {greet && <p>{greet}</p>}
+      <p>Enter your name:</p>
+      <input type="text" onChange={(e) => setGreet(e.target.value)} />
+      {greet ? <p>Hello {greet}!</p> : ""}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
